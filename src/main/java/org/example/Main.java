cbb28e8bc548c1ae75d961ef7ce7a71bc598f2be
alberto <curribifine@gmail.com>
2024-02-11 10:37:51 -0500

    se sube

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,11 @@
 package org.example;
-import java.util.*;
+//import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Resultado var = new Resultado();
         String vary= null;
         vary = var.mensaje("Hola: ");
         System.out.println(vary);
-       ;
     }
 
 }
